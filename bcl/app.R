fe19378373751e33cc7975c2699798a0ba5f986a
Meng Li <meng@phas.ubc.ca>
2018-11-21 17:02:29 -0800

    init

--- bcl/app.R
+++ bcl/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(colourpicker)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
@@ -14,10 +15,30 @@ ui <- fluidPage(
                   selected = "WINE"),
       uiOutput("countryOutput")
     ),
+    
     mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
+      # BC Liqor Store image
+      img(src = "BCLiqourStores.jpg"),
+      
+      tabsetPanel(
+      	# histogram plot 
+      	tabPanel("Plot", 
+      			 colourInput("color", "Select histogram bar colour", "red"), # add color choices
+      			 plotOutput("coolplot")
+      	),
+      
+      	tabPanel("Table",
+      		# download button
+      		downloadButton('downloadData', 'Download'), 
+   			# sort by price
+     		checkboxInput("sortPrice", label = "Sort by Price", value = FALSE), 
+  			conditionalPanel(
+  				condition = "input.sortPrice == true",
+  			 	radioButtons("sortPriceOrder", " ", choices = c("From low to high", "From high to low"))
+  			),
+      	DT::dataTableOutput("results")
+      	)
+      )
     )
   )
 )
@@ -27,32 +48,52 @@ server <- function(input, output) {
     selectInput("countryInput", "Country",
                 sort(unique(bcl$Country)),
                 selected = "CANADA")
-  })  
-  
+  })
+
   filtered <- reactive({
     if (is.null(input$countryInput)) {
       return(NULL)
-    }    
-    
+    }
+
     bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
              Type == input$typeInput,
              Country == input$countryInput
       )
+    
+    # sort price
+    if (input$sortPrice) {
+        if (input$sortPriceOrder == "From low to high") 
+        	return (arrange(bcl, Price))
+    	else 
+    		return (arrange(bcl, desc(Price)))
+    } else {
+        return (bcl)
+    }
   })
-  
+
+  # plot
   output$coolplot <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
     ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
+      geom_histogram(fill = input$color)
   })
 
-  output$results <- renderTable({
+  output$results <- DT::renderDataTable({
     filtered()
   })
+  
+  output$downloadData <- downloadHandler(
+  	filename = function() {
+ 		paste('data-', Sys.Date(), '.csv', sep='')
+  	},
+  	content = function(con) {
+  		write.csv(bcl, con)
+  	}
+  )
 }
 
 shinyApp(ui = ui, server = server)
